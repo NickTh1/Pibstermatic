7e821622cbcbd24e60328a0003de08aa6d423936
NickTh1 <NickThi@gmail.com>
2024-07-20 14:48:30 +0200

    Renamed solution

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WaveMix.Properties {
+namespace Pibstermatic.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
